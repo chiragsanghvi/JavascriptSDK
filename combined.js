c51f2aa88ec32d09ee0b33b08347e0775f9110eb
Chirag Sanghvi <chiragsanghvi@Chirag-Sanghvis-MacBook.local>
2013-05-09 18:30:59 +0530

    added node facebook functionality

--- combined.js
+++ combined.js
@@ -2571,7 +2571,7 @@ Depends on  NOTHING
 
 	"use strict";
 
-	var _facebook = function() {
+	var _browserFacebook = function() {
 
 		var _accessToken = null;
 
@@ -2631,7 +2631,69 @@ Depends on  NOTHING
 
 	};
 
-	global.Appacitive.facebook = new _facebook();
+	var _nodeFacebook = function() {
+
+		var Facebook = require('facebook-node-sdk');
+
+		var _accessToken = null;
+
+		this.FB = null;
+
+		var _app_id = null;
+
+		var _app_secret = null;
+
+		this.initialize = function (appId, appSecret) { 
+			if (!appId) throw new Error("Please provide appid");
+			if (!appSecret) throw new Error("Please provide app secret");
+			
+			_app_id = appId;
+			_app_secret = appSecret;
+		    this.FB = new Facebook({ appId: appId, secret: appSecret });
+		}
+
+		this.requestLogin = function(accessToken, onSuccess, onError) {
+			if (this.FB) {
+				_accessToken = accesstoken;
+				FB.setAccessToken(accessToken);
+			} else {
+				onError ("Intialize facebook with your appid and appsecret");
+			}
+		};
+
+		this.getCurrentUserInfo = function(onSuccess, onError) {
+			if(this.FB && _accessToken){
+				onSuccess = onSuccess || function(){};
+				onError = onError || function(){};
+				this.FB.api('/me', function(err, response) {
+					if (response) {
+						onSuccess(response);
+					} else {
+						onError("Access token is invalid");
+					}
+				});
+			} else{
+				onError("Either intialize facebook with your appid and appsecret or set accesstoken");
+			}
+		};
+
+		this.__defineGetter__('accessToken', function() {
+			return _accessToken;
+		});
+
+		this.__defineSetter__('accessToken', function(val) {
+			console.log(val);
+			_accessToken = val;
+			if(this.FB)
+				this.FB.setAccessToken(val);
+		});
+
+		this.getProfilePictureUrl = function(username) {
+			return 'https://graph.facebook.com/' + username + '/picture';
+		};
+	}
+
+	global.Appacitive.facebook = global.Appacitive.runtime.isBrowser ? new _browserFacebook() : new _nodeFacebook();
 
 })(global);(function(global) {
 
@@ -2701,7 +2763,6 @@ Depends on  NOTHING
 		this.getAllNotifications = function(pagingInfo, onSuccess, onError) {
 			onSuccess = onSuccess || function(){};
 			onError = onError || function(){};
-
 
 			if(!pagingInfo)
 				pagingInfo = { pnum: 1, psize: 20 };
--- facebook.js
+++ facebook.js
@@ -2,7 +2,7 @@
 
 	"use strict";
 
-	var _facebook = function() {
+	var _browserFacebook = function() {
 
 		var _accessToken = null;
 
@@ -62,6 +62,68 @@
 
 	};
 
-	global.Appacitive.facebook = new _facebook();
+	var _nodeFacebook = function() {
+
+		var Facebook = require('facebook-node-sdk');
+
+		var _accessToken = null;
+
+		this.FB = null;
+
+		var _app_id = null;
+
+		var _app_secret = null;
+
+		this.initialize = function (appId, appSecret) { 
+			if (!appId) throw new Error("Please provide appid");
+			if (!appSecret) throw new Error("Please provide app secret");
+			
+			_app_id = appId;
+			_app_secret = appSecret;
+		    this.FB = new Facebook({ appId: appId, secret: appSecret });
+		}
+
+		this.requestLogin = function(accessToken, onSuccess, onError) {
+			if (this.FB) {
+				_accessToken = accesstoken;
+				FB.setAccessToken(accessToken);
+			} else {
+				onError ("Intialize facebook with your appid and appsecret");
+			}
+		};
+
+		this.getCurrentUserInfo = function(onSuccess, onError) {
+			if(this.FB && _accessToken){
+				onSuccess = onSuccess || function(){};
+				onError = onError || function(){};
+				this.FB.api('/me', function(err, response) {
+					if (response) {
+						onSuccess(response);
+					} else {
+						onError("Access token is invalid");
+					}
+				});
+			} else{
+				onError("Either intialize facebook with your appid and appsecret or set accesstoken");
+			}
+		};
+
+		this.__defineGetter__('accessToken', function() {
+			return _accessToken;
+		});
+
+		this.__defineSetter__('accessToken', function(val) {
+			console.log(val);
+			_accessToken = val;
+			if(this.FB)
+				this.FB.setAccessToken(val);
+		});
+
+		this.getProfilePictureUrl = function(username) {
+			return 'https://graph.facebook.com/' + username + '/picture';
+		};
+	}
+
+	global.Appacitive.facebook = global.Appacitive.runtime.isBrowser ? new _browserFacebook() : new _nodeFacebook();
 
 })(global);
