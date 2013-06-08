d6a7bc487b59426133427184c230fa205e4198f8
chirag sanghvi <csanghvi@appacitive.com>
2013-06-08 12:10:01 +0530

    added support for parsing date and time

--- date.js
+++ date.js
@@ -3,43 +3,91 @@
   global.Appacitive.Date = {};
 
   global.Appacitive.Date.parseISODate = function (str) {
-    try{
-      var date = new Date(str); 
+    try {
+      var date = new Date(str);
       if (isNaN(date)) {
         var regexp = new RegExp("^([0-9]{1,4})-([0-9]{1,2})-([0-9]{1,2})" + "T" + "([0-9]{1,2}):([0-9]{1,2}):([0-9]{1,2})" + "(.([0-9]+))?" + "Z$");
-        if(!regexp.exec(str)) {
-           return null;
-        } else {
-           var parts = str.split('T'),
-           dateParts = parts[0].split('-'),
-           timeParts = parts[1].split('Z'),
-           timeSubParts = timeParts[0].split(':'),
-           timeSecParts = timeSubParts[2].split('.'),
-           timeHours = Number(timeSubParts[0]),
-           date = new Date();
+        var isOnlyDate = false;
+        if (!regexp.exec(str)) {
+           regexp = new RegExp("^([0-9]{1,4})-([0-9]{1,2})-([0-9]{1,2})");
+           if (!regexp.exec(str)) {
+              return null  
+           } else {
+              isOnlyDate = true;
+           }
+        }  
 
-           date.setUTCFullYear(Number(dateParts[0]));
-           date.setUTCMonth(Number(dateParts[1])-1);
-           date.setUTCDate(Number(dateParts[2]));
-           date.setUTCHours(Number(timeHours));
-           date.setUTCMinutes(Number(timeSubParts[1]));
-           date.setUTCSeconds(Number(timeSecParts[0]));
-           if (timeSecParts[1]) date.setUTCMilliseconds(Number(timeSecParts[1]));
+        var parts = str.split('T'),
+        dateParts = parts[0].split('-'),
+        timeParts = parts[1].split('Z'),
+        timeSubParts = timeParts[0].split(':'),
+        timeSecParts = timeSubParts[2].split('.'),
+        timeHours = Number(timeSubParts[0]),
+        date = new Date();
 
-           return date;
+        date.setUTCFullYear(Number(dateParts[0]));
+        date.setUTCMonth(Number(dateParts[1])-1);
+        date.setUTCDate(Number(dateParts[2]));
+        
+        if (!isOnlyDate) {
+          date.setUTCHours(Number(timeHours));
+          date.setUTCMinutes(Number(timeSubParts[1]));
+          date.setUTCSeconds(Number(timeSecParts[0]));
+          if (timeSecParts[1]) date.setUTCMilliseconds(Number(timeSecParts[1]));
         }
+        return date;
       } else {
         return date;
       }
     } catch(e) {return null;}
-  }
+  };
 
   global.Appacitive.Date.toISOString = function (date) {
     try {
       date = date.toISOString();
-      date = replace('Z','0000Z');
+      date = date.replace('Z','0000Z');
       return date;
     } catch(e) { return null;}
-  }
+  };
+
+  global.Appacitive.Date.toISODate = function(date) {
+    try {
+      date = date.toISOString()[0];
+      return date;
+    } catch(e) { return null; }
+  };
+
+  global.Appacitive.Date.toISOTime = function(date) {
+    try {
+      date = date.toISOString().split('T')[1];
+      date = date.replace('Z','0000Z');
+      return date;
+    } catch(e) { return null; }
+  };
+
+  global.Appacitive.Date.parseISOTime = function(str) {
+    try {
+      var date = new Date();
+      
+      var regexp = new RegExp("^([0-9]{1,2}):([0-9]{1,2}):([0-9]{1,2})" + "(.([0-9]+))?" + "Z$");
+      if (!regexp.exec(str)) {
+         return null;
+      }
+
+      var parts = str.split('T');
+      if (parts.length == 1) parts.push(parts[0]);
+      var timeParts = parts[1].split('Z'),
+      timeSubParts = timeParts[0].split(':'),
+      timeSecParts = timeSubParts[2].split('.'),
+      timeHours = Number(timeSubParts[0]);
+      
+      date.setUTCHours(Number(timeHours));
+      date.setUTCMinutes(Number(timeSubParts[1]));
+      date.setUTCSeconds(Number(timeSecParts[0]));
+      if (timeSecParts[1]) date.setUTCMilliseconds(Number(timeSecParts[1]));
+    
+      return date;
+    } catch(e) {return null;}
+  };
 
 })(global);
