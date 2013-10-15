92e80a5b02b7ccf5d3a88f95b35d8b0601238a2c
chirag sanghvi <csanghvi@appacitive.com>
2013-10-15 11:52:11 +0530

    merged master

--- Ti.AppacitiveSDK.js
+++ Ti.AppacitiveSDK.js
@@ -4,7 +4,7 @@
  * MIT license  : http://www.apache.org/licenses/LICENSE-2.0.html
  * Project      : https://github.com/chiragsanghvi/JavascriptSDK
  * Contact      : support@appacitive.com | csanghvi@appacitive.com
- * Build time 	: Tue Oct 15 11:47:00 IST 2013
+ * Build time 	: Tue Oct 15 11:50:11 IST 2013
  */
 
 // Add ECMA262-5 method binding if not supported natively
