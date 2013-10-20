baab9af829e8ccbd9ec061b54b7b63ba9364b473
chirag sanghvi <csanghvi@appacitive.com>
2013-10-20 17:42:24 +0530

    made changes to cloud impl

--- src/cloud.js
+++ src/cloud.js
@@ -4,40 +4,55 @@
 
 	var _cloud = function() {
 
-		this.apis = [];
+		var apis = [];
 
-		this.declare = function() {
-			if (arguments.length !== 2) throw { code: 400, message: "Invalid no. of argument provided to cloud function declare" };
+		var registerApi = function() {
+
+			if (arguments.length !== 2) throw "Invalid no. of arguments provided to cloud function declare" ;
 			
-			if (typeof arguments[0] != 'string') throw { code: 400, message: "Invalid api name provided to cloud api declare" };
-			if (typeof arguments[1] != 'function') throw { coe: 400, message: "Invalid api callback provided to cloud api declare" };
-
-			this.apis.push({ name: arguments[0], method: arguments[1] });
-		};
-
-		this.execute = function() {
-
-			if (arguments.length < 2 || typeof arguments[0] != 'string' || typeof arguments[1] != 'object')
-				throw new { code: 400, message: "Invalid list of arguments passed to incoke cloud api" };
+			if (typeof arguments[0] !== 'string') throw "Error: Provide name fo cloud api" ;
+			if (typeof arguments[1] !== 'function') throw "Error: Invalid api function provided for cloud api" ;
 
 			var name = arguments[0];
 
-			var api = this.apis.filter(function(a) {
-				return (a.name == name);
-			});
+			var regExp = /^[A-Za-z][A-Za-z_0-9]*$/;
+		    if (!regExp.test(name)) {
+		    	throw "Error: Invalid name for api function " + name;
+		    }
+
+			apis[name.toLowerCase()] = arguments[1];
+		};
+
+		this.declare = function() {
+			registerApi.apply(null, arguments);			
+		};
+
+		this.declare.toString = function() { return "function () { [native code] }"; }
+
+		var executeApi = function() {
+			if (arguments.length < 2 || typeof arguments[0] !== 'string' || typeof arguments[1] !== 'object')
+				throw new "Error: Invalid list of arguments passed to cloud api" ;
+
+			var name = arguments[0];
 
 			var request = arguments[1].request;
 			var response = arguments[1].response;
 
-			if (api.length > 0) {
-				var method = api[0].method;
+			if (typeof apis[name] == 'function') {
+				var method = apis[name];
 				setTimeout(function() {
 					method.apply(null , [request, response]);	
 				}, 0);
 			} else {
-				throw { code: '404', message: "Api with name " + arguments[0] + " doesn't exits" };
+				throw { code: '404', message: "Error: Api with name " + arguments[0] + " doesn't exits" };
 			}
 		};
+
+		this.execute = function() {
+			executeApi.apply(null, arguments);			
+		};
+
+		this.execute.toString = function() { return "function () { [native code] }"; }
 
 	};
 
